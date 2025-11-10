63826dad5eec6ab53aafc342afe0280642bbd635
Potelet Laurent <poteletlaurent@gmail.com>
2025-11-10 15:48:14 +0100

    fix(ci): Corrige l'erreur MlflowClient dans validate_mlflow.py et utilise tensorflow standard pour le CI Linux.

--- validate_mlflow.py
+++ validate_mlflow.py
@@ -1,6 +1,7 @@
 # validate_mlflow.py (À placer dans votre répertoire)
 import os
 import mlflow
+import sys
 
 # Récupère l'URI du secret GitHub
 MLFLOW_URI = os.getenv("MLFLOW_TRACKING_URI")
@@ -17,11 +18,11 @@ try:
     client = mlflow.tracking.MlflowClient()
     
     # Tente d'obtenir la liste des expériences comme test de succès
-    client.list_experiments()
+    test_experiment = client.get_experiment_by_name("mlflow_ci_test_connectivity")
     
     print(f"✅ Connexion MLflow réussie à {MLFLOW_URI}")
-    exit(0) # Succès
+    sys.exit(0) # Succès
     
 except Exception as e:
     print(f"❌ Échec de la connexion ou de l'authentification à MLflow: {e}")
-    exit(1) # Échec du Job
+    sys.exit(1) # Échec du Job
